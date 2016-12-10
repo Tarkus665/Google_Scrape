0293553bac7b15364cd9b6bc2be5936b9880f4d6
Tarkus665 <applestore@Apples-MacBook-Pro.local>
2016-12-10 11:02:15 -0600

    added a new bit

--- sound_output.py
+++ sound_output.py
@@ -10,3 +10,25 @@
 >>> sound_output.writeframes(hex_str)
 >>> sound_output.close()
 >>> 
+
+
+## integrate this automation into it...
+
+import wave
+import contextlib
+import os
+
+
+for file_name in os.listdir(os.getcwd()):
+    if file_name.endswith(".wav") or file_name.endswith(".aiff"):
+        with contextlib.closing(wave.open(file_name, 'r')) as f:
+            frames = f.getnframes()
+            rate = f.getframerate()
+            duration = frames / float(rate)
+            duration = int(duration)
+            duration = str(duration)
+            new_file_name = duration + " " + file_name
+            print(file_name)
+            os.rename(file_name, new_file_name)
+    else:
+        continue
