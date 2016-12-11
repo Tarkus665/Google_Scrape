6d81e50455eac5126f44370b00e22c6acbaf3774
Tarkus665 <applestore@Apples-MacBook-Pro.local>
2016-12-11 13:14:26 -0600

    Cleanup

--- basic wordslist creation.py
+++ basic wordslist creation.py
@@ -9,24 +9,6 @@
 
 
 
-
-#This will return the data from a google image search for "yunocchi"
-	#(replace 'yunocchi' with whatever)
-# but will also crash Python...	
-
-import urllib.request
-   
-user_agent = 'Mozilla'
-page = 'https://www.google.com/search?site=&tbm=isch&source=hp&biw=&bih=&q=yunocchi&btnG=Search+by+image'
-headers = {'User-Agent':user_agent,}
-request = urllib.request.Request(page,None,headers)
-response = urllib.request.urlopen(request)
-data = response.read()
- 
-data
-
-
-
  #This will return the DHS_words.txt file as a biiiiiig list
 
 
